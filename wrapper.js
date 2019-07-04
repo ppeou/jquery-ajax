28d67d04eba64416b4549d0431e5bbaa9cfb34d7
ppeou <ppeou@msn.com>
2019-07-03 23:52:12 -0500

    good version

--- wrapper.js
+++ wrapper.js
@@ -30,8 +30,35 @@ var isWindow = function isWindow(obj) {
   return obj != null && obj === obj.window;
 };
 
-
 var document = window.document;
+
+function buildParams( prefix, obj, traditional, add ) {
+  var name;
+
+  if ( jQuery.isArray( obj ) ) {
+    // Serialize array item.
+    jQuery.each( obj, function( i, v ) {
+      if ( traditional || rbracket.test( prefix ) ) {
+        // Treat each array item as a scalar.
+        add( prefix, v );
+
+      } else {
+        // Item is non-scalar (array or object), encode its numeric index.
+        buildParams( prefix + "[" + ( typeof v === "object" ? i : "" ) + "]", v, traditional, add );
+      }
+    });
+
+  } else if ( !traditional && jQuery.type( obj ) === "object" ) {
+    // Serialize object item.
+    for ( name in obj ) {
+      buildParams( prefix + "[" + name + "]", obj[ name ], traditional, add );
+    }
+
+  } else {
+    // Serialize scalar item.
+    add( prefix, obj );
+  }
+}
 
 function toType(obj) {
   if (obj == null) {
@@ -216,6 +243,21 @@ jQuery.extend({
 
   // Unique for each copy of jQuery on the page
   expando: "jQuery" + (version + Math.random()).replace(/\D/g, ""),
+
+  isArray: Array.isArray || function( obj ) {
+    return jQuery.type(obj) === "array";
+  },
+  isFunction: function( obj ) {
+    return jQuery.type(obj) === "function";
+  },
+  type: function( obj ) {
+    if ( obj == null ) {
+      return obj + "";
+    }
+    return typeof obj === "object" || typeof obj === "function" ?
+      class2type[ toString.call(obj) ] || "object" :
+      typeof obj;
+  },
 
   // Assume jQuery is ready without the ready module
   isReady: true,
@@ -2052,5 +2094,38 @@ jQuery.ajaxTransport(function (options) {
     }
   };
 });
+
+jQuery.param = function( a, traditional ) {
+  var prefix,
+    s = [],
+    add = function( key, value ) {
+      // If value is a function, invoke it and return its value
+      value = jQuery.isFunction( value ) ? value() : ( value == null ? "" : value );
+      s[ s.length ] = encodeURIComponent( key ) + "=" + encodeURIComponent( value );
+    };
+
+  // Set traditional to true for jQuery <= 1.3.2 behavior.
+  if ( traditional === undefined ) {
+    traditional = jQuery.ajaxSettings && jQuery.ajaxSettings.traditional;
+  }
+
+  // If an array was passed in, assume that it is an array of form elements.
+  if ( jQuery.isArray( a ) || ( a.jquery && !jQuery.isPlainObject( a ) ) ) {
+    // Serialize the form elements
+    jQuery.each( a, function() {
+      add( this.name, this.value );
+    });
+
+  } else {
+    // If traditional, encode the "old" way (the way 1.3.2 or older
+    // did it), otherwise encode params recursively.
+    for ( prefix in a ) {
+      buildParams( prefix, a[ prefix ], traditional, add );
+    }
+  }
+
+  // Return the resulting serialization
+  return s.join( "&" ).replace( r20, "+" );
+};
 
 export default jQuery;
